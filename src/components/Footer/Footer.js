aa7ce1d5b092f3ecc84cc858e1d9ff8ce505433e
arfat-beep <arfatrahman08@gmail.com>
2022-06-05 01:22:46 +0600

    footer completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillFacebook, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import { ListItem, ListTitle } from "../Technologies/TechnologiesStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel : +8801819439292">+8801819439292</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Mail</LinkTitle>
+          <LinkItem href="mailto : arfatrahman08@gmail.com">
+            arfatrahman08@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/arfat-beep">
+            <AiFillGithub size={"3rem"} />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/arfatxyz/">
+            <AiFillLinkedin size={"3rem"} />
+          </SocialIcons>
+          <SocialIcons href="https://www.facebook.com/arfat.xyz">
+            <AiFillFacebook size={"3rem"} />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
