b6ae33e455288ab96a3dc0ac8bb14c5634948409
arfat-beep <arfatrahman08@gmail.com>
2022-06-11 11:38:07 +0600

    menu content responsive 2

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -50,7 +50,7 @@ export const Div3 = styled.div`
 
 // Navigation Links
 export const NavLink = styled.a`
-  font-size: 1.7rem;
+  font-size: 1.5rem;
   line-height: 32px;
   color: rgba(255, 255, 255, 0.75);
   transition: 0.4s ease;
