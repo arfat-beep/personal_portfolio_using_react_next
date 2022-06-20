3b54c05559b1cd960fe2a26ae6d70c415d45e92a
arfat-beep <arfatrahman08@gmail.com>
2022-06-20 15:51:25 +0600

    add meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,30 @@ import { Section } from "../styles/GlobalComponents";
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <title>
+          Arfatur Rahman || Frontend Developer || Mern Stack Developer
+        </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="I'm a professional React front-end developer. I have experience working on basic HTML,CSS,JavaScript and React. I've also experience on creating WordPress website."
+        />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/stc2Ppn/arfat-xyz.jpg"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://i.ibb.co/stc2Ppn/arfat-xyz.jpg"
+        />
+
+        <meta name="author" content="Arfatur Rahman" />
+        <meta
+          name="keywords"
+          content="HTML, CSS, Display, personal portfolio, react,ReactJs,MERN, arafat,araftur rahman, arfat rahman, Metadata"
+        />
+      </Head>
       <Section grid>
         <Hero />
         <BgAnimation />
